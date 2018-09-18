c8fbc5507be5f8fcdcf30eaa6b36922b0bbb9e36
kristopherbullinger <github email address>
2018-09-18 11:04:29 -0400

    Completed lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :roster
+
+  def initialize (name)
+    @school_name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+  end
+
+  def grade(grade_number)
+    @roster[grade_number]
+  end
+
+  def sort
+    @roster.each do |grade, student_array|
+      student_array.sort!
+    end
+  end
+
+end
