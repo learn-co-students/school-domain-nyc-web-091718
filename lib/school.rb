dfe34078e9003d46aa5fe5fa4fc48866de41c781
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-18 21:52:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+#  ROSTER = {}
+  def initialize(school_name)
+    @name = name 
+  end 
+  
+  def roster
+    roster = {}
+  end 
+  
+  def add_student
+    
+  end 
+  
+end 
+ 
