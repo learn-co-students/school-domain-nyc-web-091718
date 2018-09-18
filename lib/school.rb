680636d9e24fef4ca66ebeec09f14457633a0b5f
mx@E <mglxp5@gmail.com>
2018-09-18 16:54:44 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if !!@roster[grade]
+      @roster[grade].push(name)
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    self.roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade].sort!
+    end
+    Hash[@roster.sort_by {|grade, name| grade}]
+  end
+end
