176d2e7e93c492afc9fedbff99ce726aef06dffc
srscalia <shelby.scalia@gmail.com>
2018-09-18 18:35:11 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :roster, :grade
+
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+
+  def add_student(name, grade)
+    if roster[grade].nil?
+      roster[grade]=[name]
+    else
+      roster[grade]<<name
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each do |grade, array|
+      array.sort!
+    end
+  end
+end
+
+#binding.pry
