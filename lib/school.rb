ff392d6b10bf6cce3eac0d5a75abe5dc98d71878
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-09-18 21:13:14 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
-# code here!
+# code here
+require 'pry'
+class School
+
+attr_accessor :roster
+
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(new_student, grade)
+  @grade = grade
+  if @roster.has_key?(grade)
+    @new_student = new_student
+    @roster[@grade] << @new_student
+  else
+    @new_student = []
+    @new_student << new_student
+    @roster[@grade] = @new_student
+  end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    roster = {}
+    @roster.each do |grade_num, students|
+        roster[grade_num] = students.sort
+    end
+    roster
+  end
+
+
+
+end
